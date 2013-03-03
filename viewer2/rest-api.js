741a47daeb1894203206f6e5378d1738b0270d0e
Friso van Vollenhoven <fvanvollenhoven@xebia.com>
2013-03-03 23:48:08 +0100

    Update. Still not done.

--- viewer2/rest-api.js
+++ viewer2/rest-api.js
@@ -3,7 +3,7 @@ function RestApi(location) {
 	var self = this
 	
 	function getCypherResult(query, callback) {
-		$.post(location + '/cypher',
+		$.post(self.location + '/cypher',
 			{
 				query : query,
 				params : {}
@@ -13,20 +13,20 @@ function RestApi(location) {
 					callback(data.data)
 				} else {
 					console.warn('Got unrecognized response.', data)
-					callback(undefined, { message: 'Response has no data property!', exception: ''})
+					callback(undefined, { message: 'Response has no data property!'} )
 				}
 			}).error(function(err) {
 				console.error(err)
 				if(err.responseText) {
 					callback(undefined, JSON.parse(err.responseText))
 				} else {
-					callback(undefined, {message: 'Error while talking to REST API.'})
+					callback(undefined, { message: 'Error while talking to REST API.'})
 				}
 			})
 	}
 	
 	function getBatchResult(requests, callback) {
-		$.post(location + '/batch', JSON.stringify(requests), function(response) {
+		$.post(self.location + '/batch', JSON.stringify(requests), function(response) {
 			callback(response)
 		}).error(function(err) {
 			console.error(err)
@@ -34,8 +34,162 @@ function RestApi(location) {
 		})
 	}
 
+	function neoObjectType(neoObject) {
+		return neoObject == null ? 'TABULAR' : neoObject.start && neoObject.end ? neoObject.type && neoObject.data ? 'EDGE' : 'PATH' : neoObject.data && neoObject.self ? 'NODE' : 'TABULAR'
+	}
+
+	function nodeId(input) {
+		return groupFromRegex(new RegExp("^" + self.location + "/node/(\\d+)$"), input);
+	}
+
+	function edgeId(input) {
+		return groupFromRegex(new RegExp("^" + self.location + "/relationship/(\\d+)$"), input);
+	}
+
+	function groupFromRegex(regex, input) {
+		var groups = regex.exec(input)
+		if (groups && groups.length == 2) {
+			return groups[1]
+		} else {
+			return false
+		}
+	}
+
+	function processCypherResult(result, callback) {
+		var nodes = {}
+		var edges = {}
+		var requiredEdges = {}
+		var paths = []
+		var tabular = []
+		var requests = []
+
+		var contextRegex = new RegExp("^" + self.location + "(/.*)$")
+
+		function addNeoNodeObject(node) {
+			nodes[nodeId(node.self)] = node
+		}
+
+		function addNeoEdgeObject(edge) {
+			var startId = nodeId(edge.start)
+			var endId = nodeId(edge.end)
+		
+			if (!edges[startId]) {
+				edges[startId] = {}
+			}
+			edges[startId][endId] = edge
+		
+			addNodePlaceholder(edge.start)
+			addNodePlaceholder(edge.end)
+		}
+
+		function addNeoPathObject(path) {
+			arr = []
+			for (var i = 0; i < path.nodes.length - 1; i++) {
+				var startId = nodeId(path.nodes[i])
+				var endId = nodeId(path.nodes[i+1])
+			
+				arr.push(startId, endId)
+			
+				addNodePlaceholder(path.nodes[i])
+				addEdgePlaceholder(path.relationships[i])
+			}
+			addNodePlaceholder(path.nodes[path.nodes.length - 1])
+		
+			paths.push(arr)
+		}
+
+		function addNeoTabularData(data) {
+			tabular.push(data)
+		}
+
+		function addEdgePlaceholder(edge) {
+			requiredEdges[edge] = 1
+		}
+
+		function addNodePlaceholder(node) {
+			var id = nodeId(node)
+			if (!nodes[id]) {
+				nodes[id] = node
+			}
+		}
+
+		function createResultObject() {
+			var result = {
+				nodes: nodes,
+				edges: edges
+			}
+
+			return result
+		}
+
+		$.each(result, function(rowindex, row) {
+			$.each(row, function(colindex, col) {
+				switch(neoObjectType(col)) {
+					case 'NODE':
+						addNeoNodeObject(col)
+						break;
+					case 'EDGE':
+						addNeoEdgeObject(col)
+						break;
+					case 'PATH':
+						addNeoPathObject(col)
+						break;
+					case 'TABULAR':
+						addNeoTabularData(col)
+						break;
+				}
+			})
+		})
+
+		$.each(nodes, function(id, node) {
+			if (typeof(node) == 'string') {
+				requests.push({
+					method: 'GET',
+					to: groupFromRegex(contextRegex, node)
+				})
+			}
+		})
+	
+		$.each(requiredEdges, function(edge, bogus) {
+			requests.push({
+				method: 'GET',
+				to: groupFromRegex(contextRegex, edge)
+			})
+		})
+		
+		if (requests.length > 0) {
+			getBatchResult(requests, function(missing, err) {
+				if (err) {
+					callback(undefined, err)
+				} else {
+					$.each(missing, function(key, batchResponseObject) {
+						switch(neoObjectType(batchResponseObject.body)) {
+							case 'NODE':
+								addNeoNodeObject(batchResponseObject.body)
+								break;
+							case 'EDGE':
+								addNeoEdgeObject(batchResponseObject.body)
+								break;
+						}
+					})
+
+					callback(createResultObject())
+				}
+			})
+		} else {
+			callback(createResultObject())
+		}
+
+
+	}
 
 	this.resultForQuery = function(query, callback) {
-		
+		getCypherResult(query, function(result, err) {
+			if (err) {
+				callback(undefined, err)
+			} else {
+				processCypherResult(result, callback)
+			}
+		})
 	}
 }
--- viewer2/ui.js
+++ viewer2/ui.js
@@ -1,150 +1,132 @@
 var graph
+var restApi = new RestApi('http://localhost:7474/db/data')
 
-function writeLog(message) {
-	$('#log').text(message)
-}
+function UI() {
 
-function handleError(err) {
-	alert(err.message)
-	writeLog('Query returned error!')
-	console.log(err)
-}
+	function writeLog(message) {
+		$('#log').text(message)
+	}
 
-function addQuery(query) {
-	handleResult(query, model.addQueryResult)
-}
+	function handleError(err) {
+		alert(err)
+		writeLog('Query returned error!')
+	}
 
-function replaceQuery(query) {
-	handleResult(query, model.populateFromQueryResult)
-}
+	function addQuery(query) {
+		writeLog('Running query...')
+		restApi.resultForQuery(query, handleQueryResult)
+	}
 
-function confirmHuge(result) {
-	return (result.length < 70 || confirm('Add ' + result.length + ' records to the graph?'))
-}
+	function confirmHuge(result) {
+		return (result.length < 70 || confirm('Add ' + result.length + ' records to the graph?'))
+	}
 
-function handleResult(query, method) {
-	writeLog('Running query...')
-	restApi.getCypherResult(query, function(result, err) {
-		if (err) {
-			handleError(err)
-		} else {
-			writeLog('Got response...')
-			if (result.length == 0) {
-				writeLog('Query returned empty set!')
-			} else if (confirmHuge(result)) {
-				method(result, function(err) {
-					if (err) {
-						handleError(err)
-					} else {
-						updateModel(model)
-					}
-				})
+	function handleQueryResult(result, err) {
+
+	}
+
+	function setupQueryShowHide() {
+		queryVisibleStyle = { width: '60%', height: '50%', opacity: 0.9}
+		queryHiddenStyle = { width: '25%', height: '10%', opacity: 0.3}
+
+		function hideQuery() {
+			if (!$('#queryText').is(':focus')) {
+				$('#query').animate(queryHiddenStyle, 100)
+				$('#queryText').css({ overflow: 'hidden' })
 			}
 		}
-	})
-}
 
-function setupQueryShowHide() {
-	queryVisibleStyle = { width: '60%', height: '50%', opacity: 0.9}
-	queryHiddenStyle = { width: '25%', height: '10%', opacity: 0.3}
-
-	function hideQuery() {
-		if (!$('#queryText').is(':focus')) {
-			$('#query').animate(queryHiddenStyle, 100)
-			$('#queryText').css({ overflow: 'hidden' })
+		function showQuery() {
+			$('#query').animate(queryVisibleStyle, 200)
+			$('#queryText').css({ overflow: 'auto' })
 		}
+		$('#query').hover(showQuery, hideQuery)
+		$('#query').focusout(hideQuery)
 	}
 
-	function showQuery() {
-		$('#query').animate(queryVisibleStyle, 200)
-		$('#queryText').css({ overflow: 'auto' })
-	}
-	$('#query').hover(showQuery, hideQuery)
-	$('#query').focusout(hideQuery)
-}
+	function setupLogShowHide() {
+		logVisibleStyle = { opacity: 0.9}
+		logHiddenStyle = { opacity: 0.3}
 
-function setupLogShowHide() {
-	logVisibleStyle = { opacity: 0.9}
-	logHiddenStyle = { opacity: 0.3}
-
-	function hideLog() {
-		$('#log').animate(logHiddenStyle, 100)
-	}
-
-	function showLog() {
-		$('#log').animate(logVisibleStyle, 200)
-	}
-	$('#log').hover(showLog, hideLog)
-	$('#log').focusout(hideLog)
-}
-
-function setupQueryTextCtrlEnter() {
-	$('#queryText').keypress(function(event) {
-		var keyCode = (event.which ? event.which : event.keyCode);
-		if (keyCode === 10 || keyCode == 13 && event.ctrlKey) {
-			addQuery($('#queryText').val())
+		function hideLog() {
+			$('#log').animate(logHiddenStyle, 100)
 		}
-	})
-}
 
-function main() {
+		function showLog() {
+			$('#log').animate(logVisibleStyle, 200)
+		}
+		$('#log').hover(showLog, hideLog)
+		$('#log').focusout(hideLog)
+	}
 
-	setupQueryShowHide()
-	setupLogShowHide()
-	setupQueryTextCtrlEnter()
-
-	makeGraph()
-}
-
-function makeGraph() {
-	/*var*/ graph = Viva.Graph.graph();
-
-	// Construct the graph
-	graph.addNode('anvaka', {url : 'https://secure.gravatar.com/avatar/91bad8ceeec43ae303790f8fe238164b'});
-	graph.addNode('manunt', {url : 'https://secure.gravatar.com/avatar/c81bfc2cf23958504617dd4fada3afa8'});
-	graph.addNode('thlorenz', {url : 'https://secure.gravatar.com/avatar/1c9054d6242bffd5fd25ec652a2b79cc'});
-	graph.addNode('bling', {url : 'https://secure.gravatar.com/avatar/24a5b6e62e9a486743a71e0a0a4f71af'});
-	graph.addNode('diyan', {url : 'https://secure.gravatar.com/avatar/01bce7702975191fdc402565bd1045a8?'});
-	graph.addNode('pocheptsov', {url : 'https://secure.gravatar.com/avatar/13da974fc9716b42f5d62e3c8056c718'});
-	graph.addNode('dimapasko', {url : 'https://secure.gravatar.com/avatar/8e587a4232502a9f1ca14e2810e3c3dd'});
-
-	// graph.addLink('anvaka', 'manunt');
-	// graph.addLink('anvaka', 'thlorenz');
-	graph.addLink('anvaka', 'bling');
-	graph.addLink('bling', 'manunt');
-	graph.addLink('bling', 'thlorenz');
-
-	graph.addLink('anvaka', 'diyan');
-	graph.addLink('anvaka', 'pocheptsov');
-	graph.addLink('anvaka', 'dimapasko');
-
-	// Set custom nodes appearance
-	var graphics = Viva.Graph.View.svgGraphics();
-	graphics.node(function(node) {
-		   // The function is called every time renderer needs a ui to display node
-		   return Viva.Graph.svg('image')
-				 .attr('width', 24)
-				 .attr('height', 24)
-				 .link(node.data.url); // node.data holds custom object passed to graph.addNode();
+	function setupQueryTextCtrlEnter() {
+		$('#queryText').keypress(function(event) {
+			var keyCode = (event.which ? event.which : event.keyCode);
+			if (keyCode === 10 || keyCode == 13 && event.ctrlKey) {
+				addQuery($('#queryText').val())
+			}
 		})
-		.placeNode(function(nodeUI, pos){
-			// Shift image to let links go to the center:
-			nodeUI.attr('x', pos.x - 12).attr('y', pos.y - 12);
+	}
+
+	this.createUI = function() {
+
+		setupQueryShowHide()
+		setupLogShowHide()
+		setupQueryTextCtrlEnter()
+
+		makeGraph()
+	}
+
+	function makeGraph() {
+		/*var*/ graph = Viva.Graph.graph();
+
+		// Construct the graph
+		graph.addNode('anvaka', {url : 'https://secure.gravatar.com/avatar/91bad8ceeec43ae303790f8fe238164b'});
+		graph.addNode('manunt', {url : 'https://secure.gravatar.com/avatar/c81bfc2cf23958504617dd4fada3afa8'});
+		graph.addNode('thlorenz', {url : 'https://secure.gravatar.com/avatar/1c9054d6242bffd5fd25ec652a2b79cc'});
+		graph.addNode('bling', {url : 'https://secure.gravatar.com/avatar/24a5b6e62e9a486743a71e0a0a4f71af'});
+		graph.addNode('diyan', {url : 'https://secure.gravatar.com/avatar/01bce7702975191fdc402565bd1045a8?'});
+		graph.addNode('pocheptsov', {url : 'https://secure.gravatar.com/avatar/13da974fc9716b42f5d62e3c8056c718'});
+		graph.addNode('dimapasko', {url : 'https://secure.gravatar.com/avatar/8e587a4232502a9f1ca14e2810e3c3dd'});
+
+		// graph.addLink('anvaka', 'manunt');
+		// graph.addLink('anvaka', 'thlorenz');
+		graph.addLink('anvaka', 'bling');
+		graph.addLink('bling', 'manunt');
+		graph.addLink('bling', 'thlorenz');
+
+		graph.addLink('anvaka', 'diyan');
+		graph.addLink('anvaka', 'pocheptsov');
+		graph.addLink('anvaka', 'dimapasko');
+
+		// Set custom nodes appearance
+		var graphics = Viva.Graph.View.svgGraphics();
+		graphics.node(function(node) {
+			   // The function is called every time renderer needs a ui to display node
+			   return Viva.Graph.svg('image')
+					 .attr('width', 24)
+					 .attr('height', 24)
+					 .link(node.data.url); // node.data holds custom object passed to graph.addNode();
+			})
+			.placeNode(function(nodeUI, pos){
+				// Shift image to let links go to the center:
+				nodeUI.attr('x', pos.x - 12).attr('y', pos.y - 12);
+			});
+
+		var layout = Viva.Graph.Layout.forceDirected(graph, {
+			springLength : 100,
+			springCoeff : 0.0005,
+			dragCoeff : 0.015,
+			gravity : -1.2
 		});
 
-	var layout = Viva.Graph.Layout.forceDirected(graph, {
-		springLength : 100,
-		springCoeff : 0.0005,
-		dragCoeff : 0.015,
-		gravity : -1.2
-	});
 
+		var renderer = Viva.Graph.View.renderer(graph, 
+			{
+				graphics : graphics,
+				layout : layout
+			});
 
-	var renderer = Viva.Graph.View.renderer(graph, 
-		{
-			graphics : graphics,
-			layout : layout
-		});
-
-	renderer.run();
+		renderer.run();
+	}
 }
